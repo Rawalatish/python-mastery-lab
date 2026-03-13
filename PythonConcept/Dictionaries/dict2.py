6f29f34d9c55928d41b792142d12ead81a06bf6d
atish rawal <rawalatish8@gmail.com>
2026-03-13 10:32:05 +0530

    new dict eg

---
+++ PythonConcept/Dictionaries/dict2.py
@@ -0,0 +1,14 @@
+student = {"name": "Alice",
+           "age": 58,
+           "courses": ["Math", "CS"]
+           }
+
+nn =(student.values())
+print(nn)                           # op=> dict_values(['Alice', 58, ['Math', 'CS']])
+mm = list(student.values())
+print(mm)                          # op => ['Alice', 58, ['Math', 'CS']]
+
+
+oo = tuple(student.keys())
+print(oo)                           # ('name', 'age', 'courses')
+
